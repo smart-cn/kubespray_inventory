e22ddb0e4d31dbd541b53bbf764d50bd476b3252
Your Name <you@example.com>
2021-05-18 09:41:03 -0400

    inventory refactoring

--- cluster_deploy.sh
+++
@@ -1,1 +0,0 @@
-ansible-playbook -i inventory/mycluster/hosts.yaml -u vagrant -b -v --private-key=~/.ssh/id_rsa cluster.yml --ask-become-pass
--- cluster_reset.sh
+++
@@ -1,1 +0,0 @@
-ansible-playbook -i inventory/mycluster/hosts.yaml -u vagrant -b -v --private-key=~/.ssh/id_rsa reset.yml --ask-become-pass
